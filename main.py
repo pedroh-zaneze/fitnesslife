c887bae419d13e187c778759885f348f4b0c8b4c
Pedro Henrique Zaneze Coelho <153083328+pedroh-zaneze@users.noreply.github.com>
2024-08-16 11:44:19 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -69,12 +69,12 @@ def get_root(request: Request):
     }
     return templates.TemplateResponse("painelcliente.html", view_model)
 
-@app.get("/minhapagina")
+@app.get("/perfil")
 def get_root(request: Request):
     view_model = {
         "request": request
     }
-    return templates.TemplateResponse("minhapagina.html", view_model)
+    return templates.TemplateResponse("perfil.html", view_model)
 
 @app.get("/calendario", response_class=HTMLResponse)
 async def read_calendario_current(request: Request):
@@ -138,8 +138,71 @@ def get_root(request: Request):
     }
     return templates.TemplateResponse("exercicios.html", view_model)
 
-# if __name__ == "__main__":
-#     uvicorn.run(app="main:app", port=8000, reload=True)
+@app.get("/artigos")
+def get_root(request: Request):
+    view_model = {
+        "request": request
+    }
+    return templates.TemplateResponse("artigos.html", view_model)
+
+@app.get("/configuracoes")
+def get_root(request: Request):
+    view_model = {
+        "request": request
+    }
+    return templates.TemplateResponse("configuracoes.html", view_model)
+
+@app.get("/contato")
+def get_root(request: Request):
+    view_model = {
+        "request": request
+    }
+    return templates.TemplateResponse("contato.html", view_model)
+
+@app.get("/sobre")
+def get_root(request: Request):
+    view_model = {
+        "request": request
+    }
+    return templates.TemplateResponse("sobre.html", view_model)
+
+@app.get("/professores")
+def get_root(request: Request):
+    view_model = {
+        "request": request
+    }
+    return templates.TemplateResponse("professores.html", view_model)
+
+@app.get("/perguntasfrequentes")
+def get_root(request: Request):
+    view_model = {
+        "request": request
+    }
+    return templates.TemplateResponse("perguntasfrequentes.html", view_model)
+
+@app.get("/mensagens")
+def get_root(request: Request):
+    view_model = {
+        "request": request
+    }
+    return templates.TemplateResponse("mensagens.html", view_model)
+
+@app.get("/estatisticas")
+def get_root(request: Request):
+    view_model = {
+        "request": request
+    }
+    return templates.TemplateResponse("estatisticas.html", view_model)
+
+@app.get("/dieta")
+def get_root(request: Request):
+    view_model = {
+        "request": request
+    }
+    return templates.TemplateResponse("dieta.html", view_model)
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", port=8000, reload=True)
 
 
 
